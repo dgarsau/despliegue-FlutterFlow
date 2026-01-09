d6101d523ba3f79320b0914f914048c633990b73
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-01-09 19:02:21 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -74,7 +74,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
             mainAxisSize: MainAxisSize.max,
             children: [
               Text(
-                'Hello World 2',
+                'Hello World 3',
                 style: FlutterFlowTheme.of(context).bodyMedium.override(
                       font: GoogleFonts.inter(
                         fontWeight:
